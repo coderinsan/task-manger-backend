42e1f161fd851bd4ae3130820dfe448a655bd8ef
Krishna-sm <krishna.web18@gmail.com>
2023-12-01 13:30:31 +0530

    dad

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const app = express()
+const morgan = require("morgan");
+const httpStatus = require("http-status")
+app.use(express.json())
+app.use(express.urlencoded({extended:false}))
+
+app.use(morgan("dev"))
+
+
+
+// routes
+app.use("/api/v1",require("./routes"))
+
+
+// 404 route
+app.use((req,res)=>{
+    res.status(httpStatus.NOT_FOUND).send({
+        msg:"Page not found"
+    })
+})
+
+module.exports = {
+    app
+}
